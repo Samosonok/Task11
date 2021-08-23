9db3b0fb45c49a8970718238fc3b83906933cc6c
Liliya Samosonok <samosonochek@gmail.com>
2021-08-23 14:28:55 +0300

    add method getIndex and variables

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -1,20 +1,33 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exceptions {
 
+    static int[] array;
+    static double index;
+    static double num1;
+    static double num2;
+    static double result;
+
     public static void main(String[] args) {
         getArray();
+        index = getIndex(array);
     }
 
-    private static void getArray(){
+    public static void getArray() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter array length:");
         int size = scanner.nextInt();
-        int[] array = new int[size];
+        array = new int[size];
         System.out.println("Insert array elements:");
-        for(int count = 0; count < size; count++){
+        for (int count = 0; count < size; count++) {
             array[count] = scanner.nextInt();
-            System.out.println(array[count]);
         }
+    }
+
+    public static double getIndex(int[] array) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Select a number from the array by index: " + Arrays.toString(array));
+        return scanner.nextDouble();
     }
 }
