00d7875a84067311e1704748e135391207e372b6
Liliya Samosonok <samosonochek@gmail.com>
2021-08-23 14:02:50 +0300

    add method getArray

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Exceptions {
 
+    public static void main(String[] args) {
+        getArray();
+    }
+
+    private static void getArray(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter array length:");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println("Insert array elements:");
+        for(int count = 0; count < size; count++){
+            array[count] = scanner.nextInt();
+            System.out.println(array[count]);
+        }
+    }
 }
