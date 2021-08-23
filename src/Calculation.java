b4619943e5c78f13542515138142ac6598cf0671
Liliya Samosonok <samosonochek@gmail.com>
2021-08-23 15:35:18 +0300

    add class Calculation and method for division

---
+++ src/Calculation.java
@@ -0,0 +1,6 @@
+public class Calculation {
+
+    public static int getDivisionResult(int num1, int num2){
+        return num1 / num2;
+    }
+}
--- src/Exceptions.java
+++ src/Exceptions.java
@@ -3,11 +3,14 @@ public class Exceptions {
     static int[] array;
     static int num1;
     static int num2;
-    static double result;
+    static int result;
 
     public static void main(String[] args) {
         Data.getArray();
         num1 = Data.getNumber(array);
         num2 = Data.getNumber(array);
+        System.out.println("You chose numbers: " + num1 + " and " + num2);
+        result = Calculation.getDivisionResult(num1, num2);
+        System.out.println("Result of dividing these numbers: " + result);
     }
 }
